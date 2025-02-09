b95e1f26075c2ed4de54dd23d1be0e2864222972
kiers <kierenquimosquimos@gmail.com>
2025-02-09 21:14:12 +0800

    first commit, prototyping the app

---
+++ src/components/ComputerPartsSection.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import { DialogPanel, TransitionChild } from '@headlessui/vue'
+  import { HeartIcon } from '@heroicons/vue/24/outline'
+  import { PencilIcon, PlusIcon } from '@heroicons/vue/20/solid'
+</script>
+
+<template>
+  sdsd
+</template>
