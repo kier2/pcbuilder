c4a83fa8d07ab67506bb3cea4384cfd8b25ec1a5
kiers <kierenquimosquimos@gmail.com>
2025-07-13 11:46:54 +0800

    refactor: set up emits value

--- src/components/ComputerParts.vue
+++ src/components/ComputerParts.vue
@@ -1,9 +1,40 @@
 <script setup>
-  import { DialogPanel, TransitionChild } from '@headlessui/vue'
-  import { HeartIcon } from '@heroicons/vue/24/outline'
-  import { PencilIcon, PlusIcon } from '@heroicons/vue/20/solid'
+const props = defineProps({
+  selectedItem: Object,
+});
 </script>
 
 <template>
-  sdasdasd
+  <div class="w-full">
+    <div v-if="props.selectedItem.item == 'motherboard'">
+      <h2 class="text-white">{{ props.selectedItem.item }}</h2>
+    </div>
+    <div v-else-if="props.selectedItem.item == 'cpu'">
+      <h2 class="text-white">{{ props.selectedItem.item }}</h2>
+    </div>
+    <div v-else-if="props.selectedItem.item == 'fan'">
+      <h2 class="text-white">{{ props.selectedItem.item }}</h2>
+    </div>
+    <div v-else-if="props.selectedItem.item == 'ram'">
+      <h2 class="text-white">{{ props.selectedItem.item }}</h2>
+    </div>
+    <div v-else-if="props.selectedItem.item == 'storage'">
+      <h2 class="text-white">{{ props.selectedItem.item }}</h2>
+    </div>
+    <div v-else-if="props.selectedItem.item == 'gpu'">
+      <h2 class="text-white">{{ props.selectedItem.item }}</h2>
+    </div>
+    <div v-else-if="props.selectedItem.item == 'psu'">
+      <h2 class="text-white">{{ props.selectedItem.item }}</h2>
+    </div>
+    <div v-else-if="props.selectedItem.item == 'case'">
+      <h2 class="text-white">{{ props.selectedItem.item }}</h2>
+    </div>
+    <div v-else-if="props.selectedItem.item == 'monitor'">
+      <h2 class="text-white">{{ props.selectedItem.item }}</h2>
+    </div>
+    <div v-else>
+      <h2 class="text-white"></h2>
+    </div>
+  </div>
 </template>
